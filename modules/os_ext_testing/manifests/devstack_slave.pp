39d2ad264540263ac0806c399fbd1e89bf044d2f
nickgryg <nickgryg@gmail.com>
2014-11-04 16:07:13 +0200

    My Third-party-ci

---
+++ modules/os_ext_testing/manifests/devstack_slave.pp
@@ -0,0 +1,20 @@
+# A Jenkins slave that will execute jobs that use devstack
+# to set up a full OpenStack environment for test runs.
+
+class os_ext_testing::devstack_slave (
+  $bare = true,
+  $certname = $::fqdn,
+  $ssh_key = '',
+  $python3 = false,
+  $include_pypy = false,
+) {
+  include os_ext_testing::base
+  include openstack_project::tmpcleanup
+  class { 'jenkins::slave':
+    bare         => $bare,
+    ssh_key      => $ssh_key,
+    python3      => $python3,
+    include_pypy => $include_pypy,
+  }
+  include devstack_host
+}
