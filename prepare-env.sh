a57e045e0b29b2c39c083e8a06f13ee547cb946e
nickgryg <nickgryg@gmail.com>
2014-11-05 11:57:00 +0200

    adding prepare-env.sh

---
+++ prepare-env.sh
@@ -0,0 +1,78 @@
+#! /usr/bin/env bash
+
+# Sets up a master Jenkins server and associated machinery like
+# Zuul, JJB, Gearman, etc.
+
+set -e
+
+THIS_DIR=`pwd`
+
+DATA_REPO_INFO_FILE=$THIS_DIR/.data_repo_info
+DATA_PATH=$THIS_DIR/data
+OSEXT_PATH=$THIS_DIR/third-party-ci
+OSEXT_REPO=https://github.com/nickgryg/third-party-ci
+PUPPET_MODULE_PATH="--modulepath=$OSEXT_PATH/modules:system-config/modules:/root/config/modules:/etc/puppet/modules"
+
+# Install Puppet and the OpenStack Infra Config source tree
+if [[ ! -e system-config ]]; then
+  git clone https://github.com/openstack-infra/system-config
+  sudo /bin/bash -xe system-config/install_puppet.sh
+  sudo /bin/bash -xe system-config/install_modules.sh
+fi
+
+# Clone or pull the the third-party-ci repository
+if [[ ! -d $OSEXT_PATH ]]; then
+    echo "Cloning third-party-ci repo..."
+    git clone $OSEXT_REPO $OSEXT_PATH
+fi
+
+if [[ "$PULL_LATEST_OSEXT_REPO" == "1" ]]; then
+    echo "Pulling latest third-party-ci repo master..."
+    cd $OSEXT_PATH; git checkout master && sudo git pull; cd $THIS_DIR
+fi
+
+if [[ ! -e $DATA_PATH ]]; then
+    echo "Enter the URI for the location of your config data repository. Example: https://github.com/nickgryg/third-party-ci-data"
+    read data_repo_uri
+    if [[ "$data_repo_uri" == "" ]]; then
+        echo "Data repository is required to proceed. Exiting."
+        exit 1
+    fi
+    git clone $data_repo_uri $DATA_PATH
+fi
+
+if [[ "$PULL_LATEST_DATA_REPO" == "1" ]]; then
+    echo "Pulling latest data repo master."
+    cd $DATA_PATH; git checkout master && git pull; cd $THIS_DIR;
+fi
+
+# Pulling in variables from data repository
+. $DATA_PATH/vars.sh
+
+# Validate that the upstream gerrit user and key are present in the data
+# repository
+if [[ -z $UPSTREAM_GERRIT_USER ]]; then
+    echo "Expected to find UPSTREAM_GERRIT_USER in $DATA_PATH/vars.sh. Please correct. Exiting."
+    exit 1
+else
+    echo "Using upstream Gerrit user: $UPSTREAM_GERRIT_USER"
+fi
+
+if [[ ! -e "$DATA_PATH/$UPSTREAM_GERRIT_SSH_KEY_PATH" ]]; then
+    echo "Expected to find $UPSTREAM_GERRIT_SSH_KEY_PATH in $DATA_PATH. Please correct. Exiting."
+    exit 1
+fi
+export UPSTREAM_GERRIT_SSH_PRIVATE_KEY_CONTENTS=`cat "$DATA_PATH/$UPSTREAM_GERRIT_SSH_KEY_PATH"`
+
+# Validate there is a Jenkins SSH key pair in the data repository
+if [[ -z $JENKINS_SSH_KEY_PATH ]]; then
+    echo "Expected to find JENKINS_SSH_KEY_PATH in $DATA_PATH/vars.sh. Please correct. Exiting."
+    exit 1
+elif [[ ! -e "$DATA_PATH/$JENKINS_SSH_KEY_PATH" ]]; then
+    echo "Expected to find Jenkins SSH key pair at $DATA_PATH/$JENKINS_SSH_KEY_PATH, but wasn't found. Please correct. Exiting."
+    exit 1
+else
+    echo "Using Jenkins SSH key path: $DATA_PATH/$JENKINS_SSH_KEY_PATH"
+    JENKINS_SSH_PRIVATE_KEY_CONTENTS=`sudo cat $DATA_PATH/$JENKINS_SSH_KEY_PATH`
+    JENKINS_SSH_PUBLIC_KEY_CONTENTS=`sudo cat $DATA_PATH/$JENKINS_SSH_KEY_PATH.pub`
+fi
